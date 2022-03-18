380dc0025a339f72efe0b010b8dce9433950e5a7
Fanani M. Ihsan <fanani.mi@gmail.com>
2022-03-18 07:06:15 +0700

    add descriptions

--- __manifest__.py
+++ __manifest__.py
@@ -1,26 +1,28 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "mce_hr",
+    'name': "McEasy HRS",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
-
-    'description': """
-        Long description of module's purpose
+        McEasy HRS module
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'description': """
+    """,
+
+    'author': "McEasy",
+    'website': "https://www.mceasy.co.id",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Human Resources',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': [
+        'base',
+        'muk_web_theme'
+    ],
 
     # always loaded
     'data': [
